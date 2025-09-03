b4c15cce2c3a17cc3d6bb1b4e044ce026c14b502
wakeupadi <wakeupadi96@gmail.com>
2025-09-03 15:40:08 +0530

    Update index.js, added comments for better understanding

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const { join } = require('node:path');
 const { Server } = require('socket.io');
 
 
+// Creating an Express application object.
 const app = express();
+
+//  Creates a raw https server. Incoming request are passed to app object.
 const server = createServer(app);
+
+//Fetch for new WebSocket connection and manages all the clients connected in real- time.
 const io = new Server(server);
 
+
+// .get() is a method of Express object which checks for GET method. Takes two argument: PATH and handler
+// req: is incoming request and res is our response
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
